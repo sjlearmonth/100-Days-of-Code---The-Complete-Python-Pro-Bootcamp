b07a19ba559af74904d88b98537f45f52fd3d9f8
Stephen J Learmonth <stephen.j.learmonth@gmail.com>
2024-11-12 18:02:14 +0000

    Fix typos in README.MD file for Day 2 Task 1.

--- Day-002/Task-1/task-files/task-1.py
+++ Day-002/Task-1/task-files/task-1.py
@@ -1,25 +1,25 @@
 # Print a string to the console.
 print("Hello world.")
 
-# Print an individual character from a string to the console
+# Print an individual character from a string to the console.
 print("Hello world."[0])
 
-# Print two integer values as strings concatenated together to the console
+# Print two integer values as strings concatenated together to the console.
 print("123" + "456")
 
-# Print an inteer value to the console
+# Print an integer value to the console.
 print(123)
 
 # Print the result of adding two integer values to the console.
 print(123 + 456)
 
-# Print a large integer value to the console
+# Print a large integer value to the console.
 print(123_456_789)
 
-# Print a floating-point number to the console
+# Print a floating-point number to the console.
 print(3.14159)
 
-# Print both Boolean values to the console
+# Print both Boolean values to the console.
 print(True)
 print(False)
  
