18201048ea4e16ec6206e4655b279dd2d318ef26
Stephen J Learmonth <stephen.j.learmonth@gmail.com>
2024-11-12 15:15:21 +0000

    Shorten comment in Day Task 3 Python file.

--- Day-001/Task-3/task-files/task-3.py
+++ Day-001/Task-3/task-files/task-3.py
@@ -2,7 +2,6 @@
 
 # PAUSE 1 - Update the code to add an exclamation mark.
 
-# Use string concatenation to insert the user's name between the two other strings
-# and print out the string "Hello " followed by the user's name and an exclamation
-# mark to the console.
+# Insert the user's name between two other strings "Hello " and use the ```print(...)```
+# function to print the result to the console.
 print("Hello " + input("Please type your name: ") + "!")
