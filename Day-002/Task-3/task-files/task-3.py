8f0df5aedc4a6add8d4b3754b361afe431c9c8d2
Stephen J Learmonth <stephen.j.learmonth@gmail.com>
2024-11-13 16:38:41 +0000

    Create Python file for Day 2 Task 3.

---
+++ Day-002/Task-3/task-files/task-3.py
@@ -0,0 +1,11 @@
+# Day 2 - Task 3 - Mathematical Operations
+
+# PAUSE 1.
+
+# verify that the correct result of "7" is printed out to the console.
+print(3 * 3 + 3 / 3 - 3)
+
+# PAUSE2. Change the code so that it outputs the result "3" to the console.
+print(3 * (3 + 3) / 3 - 3)
+
+
