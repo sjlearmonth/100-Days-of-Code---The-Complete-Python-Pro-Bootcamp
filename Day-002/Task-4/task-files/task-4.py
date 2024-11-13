7550b4be8cc89af6db606f1b5775e055a9c454da
Stephen J Learmonth <stephen.j.learmonth@gmail.com>
2024-11-13 18:00:13 +0000

    Upload Python file for Day 2 Task 4.

---
+++ Day-002/Task-4/task-files/task-4.py
@@ -0,0 +1,45 @@
+# Day 2 - Task 4 - Number Manipulation
+
+# PAUSE 1. Flooring a number.
+
+# Floor a float number by removing the fractional part after the decimal
+# point leaving an integer.
+print(int(3.14159))
+
+# PAUSE 2. Rounding a number.
+
+# Round some float numbers to the nearest integer.
+print(round(3.738492))
+print(round(3.14159))
+print(round(3.14159, 2))
+
+
+# PAUSE 3. Assignment Operators.
+
+# The "+=" operator giving the answer "4 + 5" = "9".
+value = 4
+value += 5
+print(value)
+
+# The "-=" operator giving the answer "10 - 3" = "7".
+value = 10
+value -= 3
+print(value)
+
+# The *= operator giving the answer "12 * 3" = "36".
+value = 12
+value *= 3
+print(value)
+
+# The /= operator giving the answer "20 / 4" = "5.0".
+value = 20
+value /= 4
+print(value)
+
+# PAUSE 4. f-Strings
+
+# Use a f-Strings to insert the contents of a variable into a string.
+age = 12
+print(f"I am {age} years old.")
+
+/Users/StephenLearmonth/PycharmProjects/"100 Days of Code - The Complete Python Pro Bootcamp2"/"Day 2"/"Number Manipulation"/task.py
